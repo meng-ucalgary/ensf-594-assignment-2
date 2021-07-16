5b0b2fdbfec04d75c1c966d779f9883c407c3789
Bhavyai Gupta <zbhavyai@gmail.com>
2021-07-17 00:53:00 +0530

    Adding source code for Quick Sort

--- src/Sort.java
+++ src/Sort.java
@@ -133,4 +133,85 @@ public class Sort {
     public static void mergeSort(int arr[]) {
         mergeSort(arr, 0, arr.length - 1);
     }
+
+    /**
+     * Method to find the partition position
+     *
+     * Source: https://www.programiz.com/dsa/quick-sort
+     * Date Accessed: 17-07-2021
+     *
+     * @param arr  an integer subarray where the position of partition is to be
+     *             found
+     * @param low  starting index of subarray
+     * @param high starting index of subarrays
+     * @return the position of the partition
+     */
+    private static int partition(int arr[], int low, int high) {
+        // choose the rightmost element as pivot
+        int pivot = arr[high];
+
+        // pointer for greater element
+        int i = (low - 1);
+
+        // traverse through all elements
+        // compare each element with pivot
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+
+                // if element smaller than pivot is found
+                // swap it with the greater element pointed by i
+                i++;
+
+                // swapping element at i with element at j
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        }
+
+        // swap the pivot element with the greater element specified by i
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        // return the position from where partition is done
+        return (i + 1);
+    }
+
+    /**
+     * Sorts the subarray <code>arr</code> inplace using Quick Sort
+     *
+     * Source: https://www.programiz.com/dsa/quick-sort
+     * Date Accessed: 17-07-2021
+     *
+     * @param arr  an integer subarray [low, high] which is to be sorted in
+     *             ascending order
+     * @param low  starting index of subarray
+     * @param high ending index of subarray
+     */
+    private static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+
+            // find pivot element such that
+            // elements smaller than pivot are on the left
+            // elements greater than pivot are on the right
+            int pi = partition(arr, low, high);
+
+            // recursive call on the left of pivot
+            quickSort(arr, low, pi - 1);
+
+            // recursive call on the right of pivot
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    /**
+     * Sorts the <code>arr</code> inplace using Quick Sort
+     *
+     * @param arr an integer array which is to be sorted in ascending order
+     */
+    public static void quickSort(int arr[]) {
+        quickSort(arr, 0, arr.length - 1);
+    }
 }
